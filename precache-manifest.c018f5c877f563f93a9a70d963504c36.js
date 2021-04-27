7c1261f598e5f37f52d59c7343e5094c87736611
carlmac <carlmac@163.com>
2021-04-27 21:10:06 +0800

    deploy

---
+++ precache-manifest.c018f5c877f563f93a9a70d963504c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a7b231b5846ac581be",
+    "url": "/jizhang-vue-local-demo/css/app.35a38cb0.css"
+  },
+  {
+    "revision": "aaa052066d3b98aaee7923766a1136ac",
+    "url": "/jizhang-vue-local-demo/index.html"
+  },
+  {
+    "revision": "39a7b231b5846ac581be",
+    "url": "/jizhang-vue-local-demo/js/app.92656438.js"
+  },
+  {
+    "revision": "3f2d3b7b9a2a45f06e6c",
+    "url": "/jizhang-vue-local-demo/js/chunk-vendors.6ea799f7.js"
+  },
+  {
+    "revision": "6a6e5c07675a568d1d24a3c5b129de92",
+    "url": "/jizhang-vue-local-demo/manifest.json"
+  },
+  {
+    "revision": "665eaec74cc7c6b198c5557d7ccc13e6",
+    "url": "/jizhang-vue-local-demo/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jizhang-vue-local-demo/robots.txt"
+  }
+]);
